a806fb9d1011b7dce6a0110771c2c05da4f53dde
Rahul Gupta <coolrahulb1995@gmail.com>
2020-05-17 16:45:49 +0530

    SessionAuthentication and     authentication_classes=[SessionAuthentication, BasicAuthentication]

--- api_basic/views.py
+++ api_basic/views.py
@@ -10,13 +10,16 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin):
     serializer_class=ArticleSerializer
     queryset=Article.objects.all()
-
     lookup_field='id'
+    authentication_classes=[SessionAuthentication, BasicAuthentication]
+    permission_classes=[IsAuthenticated]
 
     def get(self, request, id=None):
         if id:
