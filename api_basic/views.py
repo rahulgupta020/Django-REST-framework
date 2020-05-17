b8f7c54c95467fc85ee0d0454576a489fbddf842
Rahul Gupta <coolrahulb1995@gmail.com>
2020-05-17 18:19:03 +0530

    Generic Viewset and Routers

--- api_basic/views.py
+++ api_basic/views.py
@@ -16,33 +16,11 @@ from rest_framework import viewsets
 from django.shortcuts import get_object_or_404
 
 
-class ArticleViewSet(viewsets.ViewSet):
+class ArticleViewSet(viewsets.ModelViewSet):
+    serializer_class=ArticleSerializer
+    queryset=Article.objects.all()
 
-    def list(self, request):
-        articles=Article.objects.all()
-        serializer=ArticleSerializer(articles, many=True)
-        return Response(serializer.data)   
 
-    def create(self, request):
-        serializer=ArticleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def retrieve(self, request, pk=None):
-        queryset=Article.objects.all()
-        article=get_object_or_404(queryset,pk=pk)
-        serializer=ArticleSerializer(article)
-        return Response(serializer.data) 
-
-    def update(self, request, pk=None):
-        article=Article.objects.get(pk=pk)
-        serializer=ArticleSerializer(article, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
